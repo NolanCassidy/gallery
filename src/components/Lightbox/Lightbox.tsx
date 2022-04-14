5f11076d3ac3af43a9a5354cab81058afa75b779
Nolan Cassidy <nolancassidy1@Ggmail.com>
2022-04-14 10:08:48 -0700

    comment out rotation

--- src/components/Lightbox/Lightbox.tsx
+++ src/components/Lightbox/Lightbox.tsx
@@ -23,31 +23,27 @@ export const Lightbox: React.FC<LightboxProps> = ({
       <Helmet>
         <script src="movement.js" type="text/javascript" />
       </Helmet>
-      <div id="wrapper">
+      {/* <div id="wrapper">
         <div className="viewport">
           <div className="cube">
-            <div className="side">
-              <figure
-                className="perfundo__content perfundo__figure cube-image"
-                onClick={(evt) => {
-                  // Prevents clicks on the image triggering `#lightbox-untarget`.
-                  evt.stopPropagation();
-                }}
-              >
-                <img
-                  className="perfundo__image"
-                  src={asset.image_url}
-                  loading="lazy"
-                />
-                <div className="rnftg-text-black nft-text">
-                  <div>{getAssetTitle(asset)}</div>
-                  <div>{asset.collection.name}</div>
-                </div>
-              </figure>
-            </div>
+            <div className="side"> */}
+      <figure
+        className="perfundo__content perfundo__figure cube-image"
+        onClick={(evt) => {
+          // Prevents clicks on the image triggering `#lightbox-untarget`.
+          evt.stopPropagation();
+        }}
+      >
+        <img className="perfundo__image" src={asset.image_url} loading="lazy" />
+        <div className="rnftg-text-black nft-text">
+          <div>{getAssetTitle(asset)}</div>
+          <div>{asset.collection.name}</div>
+        </div>
+      </figure>
+      {/* </div>
           </div>
         </div>
-      </div>
+      </div> */}
       <a
         href="#lightbox-untarget"
         className="perfundo__close perfundo__control"
