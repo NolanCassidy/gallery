d2bcba46fb7c7234d085195d86e743050a00e1a0
Nolan Cassidy <nolancassidy1@Ggmail.com>
2022-05-09 11:21:24 -0700

    move shadow to correct side

--- src/components/Lightbox/Lightbox.tsx
+++ src/components/Lightbox/Lightbox.tsx
@@ -28,13 +28,17 @@ export const Lightbox: React.FC<LightboxProps> = ({
           <div className="cube">
             <div className="side"> */}
       <figure
-        className="perfundo__content perfundo__figure cube-image rotate"
+        className="perfundo__content perfundo__figure cube-image"
         onClick={(evt) => {
           // Prevents clicks on the image triggering `#lightbox-untarget`.
           evt.stopPropagation();
         }}
       >
-        <img className="perfundo__image" src={asset.image_url} loading="lazy" />
+        <img
+          className="perfundo__image rotate"
+          src={asset.image_url}
+          loading="lazy"
+        />
         <div className="rnftg-text-black nft-text">
           <div>{getAssetTitle(asset)}</div>
           <div>{asset.collection.name}</div>
