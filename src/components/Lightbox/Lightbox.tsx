d2a0ca9320e8490c2670d395659195267c4a7376
Nolan Cassidy <nolancassidy1@Ggmail.com>
2022-05-09 12:17:47 -0700

    remove helmet

--- src/components/Lightbox/Lightbox.tsx
+++ src/components/Lightbox/Lightbox.tsx
@@ -20,21 +20,26 @@ export const Lightbox: React.FC<LightboxProps> = ({
 }) => {
   return (
     <div id={`lightbox-${index}`} className="perfundo__overlay">
+      {/*
       <Helmet>
         <script src="movement.js" type="text/javascript" />
       </Helmet>
-      {/* <div id="wrapper">
+       <div id="wrapper">
         <div className="viewport">
           <div className="cube">
             <div className="side"> */}
       <figure
-        className="perfundo__content perfundo__figure cube-image rotate"
+        className="perfundo__content perfundo__figure cube-image"
         onClick={(evt) => {
           // Prevents clicks on the image triggering `#lightbox-untarget`.
           evt.stopPropagation();
         }}
       >
-        <img className="perfundo__image" src={asset.image_url} loading="lazy" />
+        <img
+          className="perfundo__image rotate"
+          src={asset.image_url}
+          loading="lazy"
+        />
         <div className="rnftg-text-black nft-text">
           <div>{getAssetTitle(asset)}</div>
           <div>{asset.collection.name}</div>
